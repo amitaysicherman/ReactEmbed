225801cf613a5e2990c1405b994b14956d78a961
amitay.s <amitay.sich@gmail.com>
2024-12-02 20:08:19 +0200

    ReactEmbed - official implementation.

--- common/path_manager.py
+++ common/path_manager.py
@@ -4,8 +4,18 @@ import pathlib
 BASE_DIR = str(pathlib.Path(__file__).parent.parent.resolve())
 
 data_path = os.path.join(BASE_DIR, "data")
+os.makedirs(data_path, exist_ok=True)
 item_path = os.path.join(data_path, "reactome")
+os.makedirs(item_path, exist_ok=True)
 model_path = os.path.join(data_path, "models")
+os.makedirs(model_path, exist_ok=True)
+
 scores_path = os.path.join(data_path, "scores")
+os.makedirs(scores_path, exist_ok=True)
+
 reactions_file = os.path.join(item_path, "reaction.txt")
+proteins_file = os.path.join(item_path, "proteins.txt")
+molecules_file = os.path.join(item_path, "molecules.txt")
+
 fuse_path = os.path.join(model_path, "fuse")
+os.makedirs(fuse_path, exist_ok=True)
--- preprocessing/biopax_parser.py
+++ preprocessing/biopax_parser.py
@@ -1,29 +1,86 @@
-import datetime
-from collections import defaultdict
-from typing import List, Dict
+import os
+import time
+import xml.etree.ElementTree as ET
 
 import pybiopax
+import requests
 from pybiopax.biopax import BiochemicalReaction
-from pybiopax.biopax.base import Pathway
-from pybiopax.biopax.model import BioPaxModel
-from pybiopax.biopax.util import RelationshipXref as RelXref
 from tqdm import tqdm
 
-from common.data_types import Entity, CatalystOBJ, Reaction
-
-max_complex_id = 1
+from common.path_manager import data_path, reactions_file, proteins_file, molecules_file
 
 
-def feature_parser(feature) -> str:
-    if isinstance(feature, pybiopax.biopax.FragmentFeature):
-        return ""
-    if isinstance(feature, pybiopax.biopax.ModificationFeature):
-        if not hasattr(feature, "modification_type") or feature.modification_type is None:
-            return feature.comment[0].split(" ")[0]
-        return feature.modification_type.term[0]
+def get_req(url: str, to_json=False):
+    for i in range(3):
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json() if to_json else response.text
+        print(f"Failed to retrieve url ({i}): {url}")
+        time.sleep(1)
+    return {} if to_json else ""
 
 
-def element_parser(element: pybiopax.biopax.PhysicalEntity, complex_location=None, complex_id=0):
+def from_second_line(seq):
+    lines = seq.split("\n")
+    return "".join(lines[1:]) if len(lines) > 1 else ""
+
+
+def get_smiles_from_chebi(chebi_id, default_seq=""):
+    chebi_id = chebi_id.replace("CHEBI:", "")
+    url = f"https://www.ebi.ac.uk/chebi/saveStructure.do?xml=true&chebiId={chebi_id}&imageId=0"
+    response = requests.get(url)
+    if response.status_code == 200:
+        try:
+            root = ET.fromstring(response.content)
+            smiles_tag = root.find('.//SMILES')
+            return smiles_tag.text if smiles_tag is not None else default_seq
+        except ET.ParseError:
+            print(f"Failed to parse SMILES for {chebi_id}")
+    else:
+        print(f"Failed to retrieve SMILES for {chebi_id}")
+    return default_seq
+
+
+def get_sequence(identifier, db_name):
+    db_name = db_name.lower()
+    default_seq = ""
+
+    db_handlers = {
+        "ensembl": lambda id: get_req(
+            f"https://rest.ensembl.org/sequence/id/{id}?content-type=text/plain;species=human"),
+        "embl": lambda id: from_second_line(get_req(f"https://www.ebi.ac.uk/ena/browser/api/fasta/{id}")),
+        "uniprot": lambda id: from_second_line(get_req(f"https://www.uniprot.org/uniprot/{id}.fasta")),
+        "uniprot isoform": lambda id: from_second_line(get_req(f"https://www.uniprot.org/uniprot/{id}.fasta")),
+        "chebi": lambda id: get_smiles_from_chebi(id, default_seq),
+        "guide to pharmacology": lambda id: get_req(
+            f"https://www.guidetopharmacology.org/services/ligands/{id}/structure", to_json=True).get("smiles",
+                                                                                                      default_seq),
+        "pubchem compound": lambda id: get_req(
+            f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{id}/property/CanonicalSMILES/JSON",
+            to_json=True).get("CanonicalSMILES", default_seq),
+    }
+    handler = db_handlers.get(db_name)
+    if handler is None:
+        raise ValueError(f"Unknown database name: {db_name}")
+
+    return handler(identifier)
+
+
+PROTEIN = "protein"
+MOLECULE = "molecule"
+
+
+def db_to_type(db_name):
+    proteins_data_bases = ["embl", "ensembl", "uniprot", "uniprot isoform"]
+    molecules_data_bases = ["chebi", "pubchem compound", "guide to pharmacology"]
+    db_name = db_name.lower()
+    if db_name in proteins_data_bases:
+        return PROTEIN
+    elif db_name in molecules_data_bases:
+        return MOLECULE
+
+
+def element_parser(element: pybiopax.biopax.PhysicalEntity):
     if not hasattr(element, "entity_reference") or not hasattr(element.entity_reference, "xref"):
         if hasattr(element, "xref"):
             for xref in element.xref:
@@ -34,6 +91,9 @@ def element_parser(element: pybiopax.biopax.PhysicalEntity, complex_location=Non
             else:
                 ref_db = "0"
                 ref_id = element.display_name
+        else:
+            ref_db = "0"
+            ref_id = element.display_name
 
     elif len(element.entity_reference.xref) > 1:
         print(len(element.entity_reference.xref), "xrefs")
@@ -42,131 +102,83 @@ def element_parser(element: pybiopax.biopax.PhysicalEntity, complex_location=Non
     else:
         ref_db = element.entity_reference.xref[0].db
         ref_id = element.entity_reference.xref[0].id
-
-    name = element.display_name
-    if complex_location:
-        location = complex_location
-    else:
-        if not hasattr(element, "cellular_location") or not hasattr(element.cellular_location, "term"):
-            location = ""
-        else:
-            location = element.cellular_location.term[0]
-
-    features = list(element.feature)
-    modifications = [feature_parser(f) for f in features]
-    modifications = [f for f in modifications if f != ""]
-    modifications = tuple(modifications)
-    return Entity(name, ref_db, ref_id, location, modifications, complex_id)
+    return ref_db, ref_id
 
 
-def add_protein_or_complex(entity, complex_location=None, complex_id=0):
+def get_all_elements(entity):
     elements = []
     if entity.member_physical_entity:
-        entity = entity.member_physical_entity[0]  # TODO: get all set elements
-
+        for entity in entity.member_physical_entity:
+            elements.extend(get_all_elements(entity))
     if isinstance(entity, pybiopax.biopax.Complex):
-
-        if complex_location is None:
-            global max_complex_id
-            complex_id = max_complex_id
-            max_complex_id += 1
-            complex_location = entity.cellular_location.term[0]
         for entity in entity.component:
-            elements.extend(add_protein_or_complex(entity, complex_location, complex_id))
+            elements.extend(get_all_elements(entity))
     elif isinstance(entity, pybiopax.biopax.PhysicalEntity):
-        elements.append(element_parser(entity, complex_location, complex_id))
-
+        elements.append(element_parser(entity))
     else:
         print("Unknown entity", type(entity))
     return elements
 
 
-def catalysis_parser(catalysis_list: List[pybiopax.biopax.Catalysis]) -> List[CatalystOBJ]:
-    results = []
-    for catalysis in catalysis_list:
-        assert len(catalysis.controller) == 1, "More than one controller"
-        catalysis_entities = add_protein_or_complex(catalysis.controller[0])
-        catalysis_activity = catalysis.xref[0].id
-        results.append(CatalystOBJ(catalysis_entities, catalysis_activity))
-    return results
+def elements_to_prot_mols(elements, proteins_to_id, molecules_to_id):
+    proteins = []
+    molecules = []
+    for db, db_id in elements:
+        type_ = db_to_type(db)
+        if type_ == PROTEIN:
+            if db_id not in proteins_to_id:
+                proteins_to_id[(db, db_id)] = len(proteins_to_id)
+            proteins.append(proteins_to_id[(db, db_id)])
+        elif type_ == MOLECULE:
+            if db_id not in molecules_to_id:
+                molecules_to_id[(db, db_id)] = len(molecules_to_id)
+            molecules.append(molecules_to_id[(db, db_id)])
+    return proteins, molecules
 
 
-def get_reactome_id(reaction: BiochemicalReaction) -> str:
-    if not hasattr(reaction, "xref"):
-        return "0"
-    for xref in reaction.xref:
-        if "Reactome" in xref.db:
-            return xref.id
-    return "0"
+def save_all_sequences(data_dict, output_file):
+    from joblib import Parallel, delayed
+
+    def fetch_sequence(db, db_id):
+        return get_sequence(db_id, db)
+
+    all_seq = Parallel(n_jobs=-1)(
+        delayed(fetch_sequence)(db, db_id) for (db, db_id), _ in tqdm(data_dict.items(), desc="Fetching sequences"))
+    with open(output_file, "w") as f:
+        f.write("\n".join(all_seq))
 
 
-def get_reaction_date(reaction: BiochemicalReaction, format='%Y-%m-%d',
-                      default_date=datetime.date(1970, 1, 1)) -> datetime.date:
-    if not hasattr(reaction, "comment"):
-        return default_date
-    comments = [c.split(", ")[-1].split(" ")[0] for c in reaction.comment if "Authored" in c]
-    if not comments:
-        return default_date
-    date_str = comments[0]
-    try:
-        date = datetime.datetime.strptime(date_str, format).date()
-    except:
-        date = default_date
-        print(f"Error in date: {date_str}")
+def main():
+    proteins_to_id = {}
+    molecules_to_id = {}
 
-    return date
+    input_file = os.path.join(data_path, "biopax", "Homo_sapiens.owl")
+    if os.path.exists(reactions_file):
+        os.remove(reactions_file)
+    model = pybiopax.model_from_owl_file(input_file)
+    reactions = []
+    for i, reaction in tqdm(enumerate(model.get_objects_by_type(pybiopax.biopax.BiochemicalReaction))):
+        assert reaction.conversion_direction == "LEFT-TO-RIGHT"
+        elements = []
+        for entity in reaction.left:
+            elements.extend(get_all_elements(entity))
 
-
-def reactions_to_biological_process(model: BioPaxModel) -> Dict[str, List[str]]:
-    pathways = list(model.get_objects_by_type(Pathway))
-    inv_mapping = {}
-    for pathway in pathways:
-        for c in pathway.pathway_component:
-            inv_mapping[c] = pathway
-    mappings = defaultdict(list)
-    for reaction in model.get_objects_by_type(BiochemicalReaction):
-        parent = reaction
-        while True:
-            if parent not in inv_mapping:
-                go_relations = []
-                break
-            parent = inv_mapping[parent]
-            go_relations = [ref.id for ref in parent.xref if isinstance(ref, RelXref) and ref.db == "GENE ONTOLOGY"]
-            if len(go_relations) > 0:
-                break
-
-        mappings[reaction.uid].extend(go_relations)
-    return mappings
+        for entity in reaction.right:
+            elements.extend(get_all_elements(entity))
+        proteins, molecules = elements_to_prot_mols(elements, proteins_to_id, molecules_to_id)
+        proteins, molecules = list(set(proteins)), list(set(molecules))
+        reactions.append(",".join([str(p) for p in proteins]) + " " + ",".join([str(m) for m in molecules]))
+    with open(reactions_file, "w") as f:
+        f.write("\n".join(reactions))
+    with open(proteins_file, "w") as f:
+        for k, v in proteins_to_id.items():
+            f.write(f'{k[0]} {k[1]} {v}\n')
+    with open(molecules_file, "w") as f:
+        for k, v in molecules_to_id.items():
+            f.write(f'{k[0]} {k[1]} {v}\n')
+    save_all_sequences(proteins_to_id, proteins_file.replace(".txt", "_sequences.txt"))
+    save_all_sequences(molecules_to_id, molecules_file.replace(".txt", "_sequences.txt"))
 
 
 if __name__ == "__main__":
-    from common.path_manager import data_path, reactions_file
-    import os
-
-    input_file = os.path.join(data_path, "biopax", "Homo_sapiens.owl")
-    output_file = reactions_file
-    if os.path.exists(output_file):
-        os.remove(output_file)
-    model = pybiopax.model_from_owl_file(input_file)
-    reaction_to_go = reactions_to_biological_process(model)
-    reactions = list(model.get_objects_by_type(pybiopax.biopax.BiochemicalReaction))
-    all_catalysis = list(model.get_objects_by_type(pybiopax.biopax.Catalysis))
-    print(len(reactions))
-    for i, reaction in tqdm(enumerate(reactions)):
-        assert reaction.conversion_direction == "LEFT-TO-RIGHT"
-        left_elements = []
-        for entity in reaction.left:
-            left_elements.extend(add_protein_or_complex(entity))
-
-        right_elements = []
-        for entity in reaction.right:
-            right_elements.extend(add_protein_or_complex(entity))
-        catalys_activities = [c for c in all_catalysis if c.controlled == reaction]
-        catalys_activities = catalysis_parser(catalys_activities)
-        date = get_reaction_date(reaction)
-        reactome_id = get_reactome_id(reaction)
-        biological_process = reaction_to_go[reaction.uid]
-        reaction_obj = Reaction(reaction.name[0], left_elements, right_elements, catalys_activities, date, reactome_id,
-                                biological_process)
-        with open(output_file, "a") as f:
-            f.write(f'{reaction_obj.to_dict()}\n')
+    main()
