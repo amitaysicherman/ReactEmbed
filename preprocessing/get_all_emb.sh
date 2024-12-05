a1fca928eb714a53fe6496d2fa6cefdf3d69a854
amitay.s <amitay.sich@gmail.com>
2024-12-05 10:21:29 +0200

    ReactEmbed - official implementation.

--- preprocessing/get_all_emb.sh
+++ preprocessing/get_all_emb.sh
@@ -9,5 +9,8 @@ NAMES=("ProtBert" "ChemBERTa" "MoLFormer" "esm3-small" "esm3-medium" "esm3-large
 
 
 NAME=${NAMES[$SLURM_ARRAY_TASK_ID - 1]}
-huggingface-cli login --token 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
+# if name is esm3-small, esm3-medium load the model from huggingface
+if [ $NAME == "esm3-small" ] || [ $NAME == "esm3-medium" ]; then
+    huggingface-cli login --token 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
+fi
 python preprocessing/seq_to_vec.py --model $NAME
