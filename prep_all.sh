b00ca3ef265a96522f4b7e4096688b6e140c777d
amitay.s <amitay.sich@gmail.com>
2024-12-17 17:53:59 +0200

    ReactEmbed - official implementation.

--- prep_all.sh
+++ prep_all.sh
@@ -14,23 +14,15 @@ done
 
 mol_task=("BACE" "BBBP" "CEP" "ClinTox" "Delaney" "FreeSolv" "HIV" "Lipophilicity" "Malaria" "SIDER" "Tox21")
 for m_task in "${mol_task[@]}"; do
-  for m_model in "${molecule_models[@]}"; do
-    python eval_tasks/prep_tasks.py --task "$m_task" --m_model "$m_model"
-  done
+    python eval_tasks/prep_tasks_seqs.py --task "$m_task"
 done
 
 prot_task=("BetaLactamase" "Fluorescence" "Stability" "BinaryLocalization" "HumanPPI" "YeastPPI" "PPIAffinity")
 for p_task in "${prot_task[@]}"; do
-  for p_model in "${protein_models[@]}"; do
-    python eval_tasks/prep_tasks.py --task "$p_task" --p_model "$p_model"
-  done
+    python eval_tasks/prep_tasks_seqs.py --task "$p_task"
 done
 
 lig_task=("BindingDB" "PDBBind" "DrugBank" "Davis")
 for l_task in "${lig_task[@]}"; do
-  for p_model in "${protein_models[@]}"; do
-    for m_model in "${molecule_models[@]}"; do
-      python eval_tasks/prep_tasks.py --task "$l_task" --p_model "$p_model" --m_model "$m_model"
-    done
-  done
+    python eval_tasks/prep_tasks_seqs.py --task "$l_task"
 done
