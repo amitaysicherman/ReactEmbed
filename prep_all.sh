54ee7b0b0747b99cc7e706f78c3dea1f681792ae
amitay.s <amitay.sich@gmail.com>
2024-12-17 14:26:27 +0200

    ReactEmbed - official implementation.

--- prep_all.sh
+++ prep_all.sh
@@ -12,18 +12,25 @@ for p_model in "${protein_models[@]}"; do
     python preprocessing/seq_to_vec.py --model "$p_model"
 done
 
-
-# Define tasks
-tasks=("BACE" "BBBP" "CEP" "ClinTox" "Delaney" "FreeSolv" "HIV" "Lipophilicity" "Malaria" "SIDER" "Tox21" \
-"BetaLactamase" "Fluorescence" "Stability" "BinaryLocalization" "HumanPPI" "YeastPPI" "PPIAffinity" \
-"BindingDB" "PDBBind" "DrugBank" "Davis")
-
-# Step 2: Evaluate tasks for each molecule-protein pair
-for task in "${tasks[@]}"; do
+mol_task=("BACE" "BBBP" "CEP" "ClinTox" "Delaney" "FreeSolv" "HIV" "Lipophilicity" "Malaria" "SIDER" "Tox21")
+for m_task in "${mol_task[@]}"; do
   for m_model in "${molecule_models[@]}"; do
-    for p_model in "${protein_models[@]}"; do
-      echo "Running task: $task with molecule model: $m_model and protein model: $p_model"
-      python eval_tasks/prep_tasks.py --task "$task" --p_model "$p_model" --m_model "$m_model"
+    python preprocessing/prep_tasks.py --task "$m_task" --m_model "$m_model"
+  done
+done
+
+prot_task=("BetaLactamase" "Fluorescence" "Stability" "BinaryLocalization" "HumanPPI" "YeastPPI" "PPIAffinity")
+for p_task in "${prot_task[@]}"; do
+  for p_model in "${protein_models[@]}"; do
+    python preprocessing/prep_tasks.py --task "$p_task" --p_model "$p_model"
+  done
+done
+
+lig_task=("BindingDB" "PDBBind" "DrugBank" "Davis")
+for l_task in "${lig_task[@]}"; do
+  for p_model in "${protein_models[@]}"; do
+    for m_model in "${molecule_models[@]}"; do
+      python preprocessing/prep_tasks.py --task "$l_task" --p_model "$p_model" --m_model "$m_model"
     done
   done
 done
