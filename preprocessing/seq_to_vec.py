4a0dd46df25ceb40c74669a00dfe208b54856f27
amitay.s <amitay.sich@gmail.com>
2024-12-22 17:32:42 +0200

    ReactEmbed - official implementation.

--- preprocessing/seq_to_vec.py
+++ preprocessing/seq_to_vec.py
@@ -5,7 +5,13 @@ import numpy as np
 import torch
 from esm.models.esmc import ESMC
 from esm.sdk.api import ESMProtein, LogitsConfig
-from transformers import AutoModel, AutoTokenizer, BertModel, BertTokenizer
+from rdkit import Chem
+from torchdrug import models, layers, data, transforms
+from torchdrug.layers import geometry
+from transformers import AutoModel, BertModel, BertTokenizer
+from transformers import AutoTokenizer, EsmForProteinFolding
+from transformers.models.esm.openfold_utils.feats import atom14_to_atom37
+from transformers.models.esm.openfold_utils.protein import to_pdb, Protein as OFProtein
 
 from common.path_manager import proteins_file, molecules_file, item_path
 
@@ -15,6 +21,77 @@ name_to_hf_cp = {
     "ChemBERTa": "seyonec/ChemBERTa-zinc-base-v1",
     "MoLFormer": "ibm/MoLFormer-XL-both-10pct"
 }
+
+
+def fold_to_pdb(outputs):
+    final_atom_positions = atom14_to_atom37(outputs["positions"][-1], outputs)
+    outputs = {k: v.to("cpu").numpy() for k, v in outputs.items()}
+    final_atom_positions = final_atom_positions.cpu().numpy()
+    final_atom_mask = outputs["atom37_atom_exists"]
+    pdbs = []
+    for i in range(outputs["aatype"].shape[0]):
+        aa = outputs["aatype"][i]
+        pred_pos = final_atom_positions[i]
+        mask = final_atom_mask[i]
+        resid = outputs["residue_index"][i] + 1
+        pred = OFProtein(
+            aatype=aa,
+            atom_positions=pred_pos,
+            atom_mask=mask,
+            residue_index=resid,
+            b_factors=outputs["plddt"][i],
+            chain_index=outputs["chain_index"][i] if "chain_index" in outputs else None,
+        )
+        pdbs.append(to_pdb(pred))
+    return pdbs
+
+
+class GearNet3Embedder:
+    def __init__(self, gearnet_cp_file="data/models/gearnet/mc_gearnet_edge.pth"):
+        self.fold_tokenizer = AutoTokenizer.from_pretrained("facebook/esmfold_v1")
+        self.fold_model = EsmForProteinFolding.from_pretrained("facebook/esmfold_v1")
+        self.fold_model = self.fold_model.to(device).eval()
+        self.gearnet_model = models.GearNet(input_dim=21, hidden_dims=[512, 512, 512, 512, 512, 512], num_relation=7,
+                                            edge_input_dim=59,
+                                            num_angle_bin=8,
+                                            batch_norm=True, concat_hidden=True, short_cut=True, readout="sum").to(
+            device).eval()
+        checkpoint = torch.load(gearnet_cp_file)
+        self.gearnet_model.load_state_dict(checkpoint)
+        self.graph_construction_model = layers.GraphConstruction(node_layers=[geometry.AlphaCarbonNode()],
+                                                                 edge_layers=[
+                                                                     geometry.SpatialEdge(radius=10.0, min_distance=5),
+                                                                     geometry.KNNEdge(k=10, min_distance=5),
+                                                                     geometry.SequentialEdge(max_distance=2)],
+                                                                 edge_feature="gearnet")
+
+    def fold_seq(self, seq: str, output_file):
+        tokenized_input = self.fold_tokenizer([seq], return_tensors="pt", add_special_tokens=False)['input_ids']
+        tokenized_input = tokenized_input.to(device)
+        with torch.no_grad():
+            output = self.fold_model(tokenized_input)
+        pdbs = fold_to_pdb(output)
+        with open(output_file, 'w') as f:
+            f.write(pdbs[0])
+
+    def to_vec(self, seq: str, fold_tmp_file="tmp.pdb"):
+        self.fold_seq(seq, fold_tmp_file)
+        mol = Chem.MolFromPDBFile(fold_tmp_file, sanitize=False)
+        os.remove(fold_tmp_file)
+        if mol is None:
+            raise ValueError("RDKit cannot read PDB file 'tmp.pdb'")
+        protein = data.Protein.from_molecule(mol)
+        truncate_transform = transforms.TruncateProtein(max_length=550, random=False)
+        protein_view_transform = transforms.ProteinView(view="residue")
+        transform = transforms.Compose([truncate_transform, protein_view_transform])
+        protein = {"graph": protein}
+        protein = transform(protein)
+        protein = protein["graph"]
+        protein = data.Protein.pack([protein])
+        protein = self.graph_construction_model(protein)
+        output = self.gearnet_model(protein.cuda(), protein.node_feature.float().cuda())
+        output = output['node_feature'].mean(dim=0)
+        return output.cpu().numpy().flatten()
 
 
 class Esm3Embedder:
@@ -104,6 +181,8 @@ class SeqToVec:
         elif model_name in ["esm3-small", "esm3-medium"]:
             size = model_name.split("-")[-1]
             self.model = Esm3Embedder(size)
+        elif model_name == "GearNet":
+            self.model = GearNet3Embedder()
         else:
             raise ValueError(f"Unknown model: {model_name}")
 
