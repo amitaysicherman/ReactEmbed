92431be22a3665dd8e95724c989c3e7ce5d34aa1
amitay.s <amitay.sich@gmail.com>
2024-12-23 13:58:46 +0200

    ReactEmbed - official implementation.

--- prep_array.sh
+++ prep_array.sh
@@ -3,58 +3,80 @@
 #SBATCH --mem=64G
 #SBATCH --requeue
 #SBATCH --gres=gpu:L40:1
-#SBATCH --array=1-11
+#SBATCH --array=1-414
 
-#pathbank reactome reactome_all
 DATA_NAMES_ARRAY=("pathbank" "reactome" "reactome_all")
 PROTEINS_MODELS_ARRAY=("GearNet" "ProtBert" "esm3-small", "esm3-medium")
 MOLECULES_MODELS_ARRAY=("MolCLR" "ChemBERTa" "MoLFormer")
+mol_task=("BACE" "BBBP" "CEP" "ClinTox" "Delaney" "FreeSolv" "HIV" "Lipophilicity" "Malaria" "SIDER" "Tox21")
+prot_task=("BetaLactamase" "Fluorescence" "Stability" "BinaryLocalization" "HumanPPI" "YeastPPI" "PPIAffinity")
+lig_task=("BindingDB" "PDBBind" "DrugBank" "Davis")
+all_tasks=("${mol_task[@]}" "${prot_task[@]}" "${lig_task[@]}")
 
-for data_name in "${DATA_NAMES_ARRAY[@]}" ; do
-  echo python preprocessing/biopax_parser.py --data_name $data_name
-done
+commands_array=()
+
+#for data_name in "${DATA_NAMES_ARRAY[@]}" ; do
+#  echo python preprocessing/biopax_parser.py --data_name $data_name
+#done
+
+#for task in "${all_tasks[@]}"; do
+#    python eval_tasks/prep_tasks_seqs.py --task "$task"
+#done
+
 
 for data_name in "${DATA_NAMES_ARRAY[@]}" ; do
   for p_model in "${PROTEINS_MODELS_ARRAY[@]}" ;do
-    echo python preprocessing/seq_to_vec.py --data_name $data_name --model $p_model
-  done  
+    cmd="python preprocessing/seq_to_vec.py --data_name $data_name --model $p_model"
+    commands_array+=($cmd)
+  done
   for m_model in "${MOLECULES_MODELS_ARRAY[@]}" ;do
-      echo python preprocessing/seq_to_vec.py --data_name $data_name --model $m_model
+    cmd="python preprocessing/seq_to_vec.py --data_name $data_name --model $m_model"
+    commands_array+=($cmd)
   done
   for p_model in "${PROTEINS_MODELS_ARRAY[@]}" ; do
     for m_model in "${MOLECULES_MODELS_ARRAY[@]}" ; do
-      echo python contrastive_learning/trainer.py --data_name $data_name --p_model $p_model --m_model $m_model
+      cmd="python contrastive_learning/trainer.py --data_name $data_name --p_model $p_model --m_model $m_model"
+      commands_array+=($cmd)
     done
   done
 done
 
 
 for m_model in "${molecule_models[@]}"; do
-    echo python preprocessing/seq_to_vec.py --model "$m_model"
+    cmd="python preprocessing/seq_to_vec.py --model $m_model"
+    commands_array+=($cmd)
 done
 for p_model in "${protein_models[@]}"; do
-    echo python preprocessing/seq_to_vec.py --model "$p_model"
+    cmd="python preprocessing/seq_to_vec.py --model $p_model"
+    commands_array+=($cmd)
 done
 
-mol_task=("BACE" "BBBP" "CEP" "ClinTox" "Delaney" "FreeSolv" "HIV" "Lipophilicity" "Malaria" "SIDER" "Tox21")
 for m_task in "${mol_task[@]}"; do
   for m_model in "${molecule_models[@]}"; do
-    echo python eval_tasks/prep_tasks_vecs.py --task "$m_task" --m_model "$m_model"
+    cmd="python eval_tasks/prep_tasks_vecs.py --task $m_task --m_model $m_model"
+    commands_array+=($cmd)
   done
 done
 
-prot_task=("BetaLactamase" "Fluorescence" "Stability" "BinaryLocalization" "HumanPPI" "YeastPPI" "PPIAffinity")
 for p_task in "${prot_task[@]}"; do
   for p_model in "${protein_models[@]}"; do
-    echo python eval_tasks/prep_tasks_vecs.py --task "$p_task" --p_model "$p_model"
+    cmd="python eval_tasks/prep_tasks_vecs.py --task $p_task --p_model $p_model"
+    commands_array+=($cmd)
   done
 done
 
-lig_task=("BindingDB" "PDBBind" "DrugBank" "Davis")
 for l_task in "${lig_task[@]}"; do
   for p_model in "${protein_models[@]}"; do
     for m_model in "${molecule_models[@]}"; do
-      echo python eval_tasks/prep_tasks_vecs.py --task "$l_task" --p_model "$p_model" --m_model "$m_model"
+      cmd="python eval_tasks/prep_tasks_vecs.py --task $l_task --p_model $p_model --m_model $m_model"
+      commands_array+=($cmd)
     done
   done
 done
+
+# print number of commands
+echo "Number of commands: ${#commands_array[@]}"
+# run the command based on the $SLURM_ARRAY_TASK_ID-1
+cmd_to_run=${commands_array[$SLURM_ARRAY_TASK_ID-1]}
+echo "Running command: $cmd_to_run"
+eval $cmd_to_run
