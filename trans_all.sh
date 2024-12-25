71138718629f563d233c32d84c49eb5f6d82ef13
amitay.s <amitay.sich@gmail.com>
2024-12-25 21:22:43 +0200

    ReactEmbed - official implementation.

---
+++ trans_all.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --mem=64G
+#SBATCH --requeue
+#SBATCH --gres=gpu:1
+#SBATCH --array=1-4
+
+configs="--metric f1_max |\
+  --metric auprc |\
+  --metric acc |\
+  --metric f1_max"
+
+
+models="--p_model ProtBert --m_model ChemBERTa --fusion_name data/reactome/model/ProtBert-ChemBERTa-1-256-0.3-1-5e-05-256-0.0/  |\
+--p_model esm3-medium --m_model ChemBERTa --fusion_name data/reactome/model/esm3-medium-ChemBERTa-1-256-0.3-1-5e-05-256-0.0/  |\
+--p_model esm3-small --m_model ChemBERTa --fusion_name data/reactome/model/esm3-small-ChemBERTa-1-256-0.3-1-5e-05-256-0.0/  |\
+--p_model ProtBert --m_model ChemBERTa --fusion_name data/reactome/model/ProtBert-ChemBERTa-2-256-0.3-10-5e-05-256-0.0/  |\
+--p_model esm3-medium --m_model ChemBERTa --fusion_name data/reactome/model/esm3-medium-ChemBERTa-2-256-0.3-10-5e-05-256-0.0/  |\
+--p_model esm3-small --m_model ChemBERTa --fusion_name data/reactome/model/esm3-small-ChemBERTa-2-256-0.3-10-5e-05-256-0.0/  |\
+--p_model ProtBert --m_model MoLFormer --fusion_name data/reactome/model/ProtBert-MoLFormer-1-256-0.3-1-5e-05-256-0.0/  |\
+--p_model esm3-medium --m_model MoLFormer --fusion_name data/reactome/model/esm3-medium-MoLFormer-1-256-0.3-1-5e-05-256-0.0/  |\
+--p_model esm3-small --m_model MoLFormer --fusion_name data/reactome/model/esm3-small-MoLFormer-1-256-0.3-1-5e-05-256-0.0/  |\
+--p_model ProtBert --m_model MoLFormer --fusion_name data/reactome/model/ProtBert-MoLFormer-2-256-0.3-10-5e-05-256-0.0/  |\
+--p_model esm3-medium --m_model MoLFormer --fusion_name data/reactome/model/esm3-medium-MoLFormer-2-256-0.3-10-5e-05-256-0.0/  |\
+--p_model esm3-small --m_model MoLFormer --fusion_name data/reactome/model/esm3-small-MoLFormer-2-256-0.3-10-5e-05-256-0.0/  |\
+--p_model ProtBert --m_model MolCLR --fusion_name data/reactome/model/ProtBert-MolCLR-1-256-0.3-1-5e-05-256-0.0/  |\
+--p_model esm3-medium --m_model MolCLR --fusion_name data/reactome/model/esm3-medium-MolCLR-1-256-0.3-1-5e-05-256-0.0/  |\
+--p_model esm3-small --m_model MolCLR --fusion_name data/reactome/model/esm3-small-MolCLR-1-256-0.3-1-5e-05-256-0.0/  |\
+--p_model ProtBert --m_model MolCLR --fusion_name data/reactome/model/ProtBert-MolCLR-2-256-0.3-10-5e-05-256-0.0/  |\
+--p_model esm3-medium --m_model MolCLR --fusion_name data/reactome/model/esm3-medium-MolCLR-2-256-0.3-10-5e-05-256-0.0/  |\
+--p_model esm3-small --m_model MolCLR --fusion_name data/reactome/model/esm3-small-MolCLR-2-256-0.3-10-5e-05-256-0.0/"
+IFS='|' read -ra config_array <<< "$configs"
+config=${config_array[$((SLURM_ARRAY_TASK_ID - 1))]}
+
+IFS='|' read -ra models_array <<< "$models"
+
+eval "$(conda shell.bash hook)"
+conda activate ReactEmbedTorchDrug
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+for model in "${models_array[@]}"
+do
+    python transferrin/train_mol_pred_prot.py $config $model
+done
+
+
--- transferrin/train_mol_pred_prot.py
+++ transferrin/train_mol_pred_prot.py
@@ -1,43 +1,62 @@
 import torch
 
 from contrastive_learning.model import ReactEmbedModel
-from eval_tasks.models import LinFuseModel
 from eval_tasks.trainer import main as trainer_task_main
-from transferrin.utils import PreprocessManager, get_go_description, find_top_n_combinations
+from transferrin.utils import PreprocessManager, find_top_n_combinations
 
 transferrin_id = "P02787"
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-p_model = "esm3-medium"
-m_model = "MoLFormer"
-preprocess = PreprocessManager(p_model=p_model, reactome=True)
-fuse_base = f"data/reactome/model/{p_model}-{m_model}-2-256-0.3-10-0.001-8192-0.0"
-model: LinFuseModel = None
-score, model = trainer_task_main(use_fuse=True, use_model=False, bs=32, lr=1e-4, drop_out=0.1, hidden_dim=128,
-                                 task_name="BBBP", fuse_base=fuse_base, mol_emd=m_model, protein_emd=p_model,
-                                 n_layers=2, metric="auc", max_no_improve=15, return_model=True)
-print("Model loaded")
-print("score", score)
-vecs = preprocess.get_vecs()
-proteins = torch.tensor(vecs).to(device)
-fuse_model: ReactEmbedModel = model.fuse_model
-fuse_model.eval()
-x = fuse_model.dual_forward(proteins, "P")
-pred = model.layers(x)
 
-res = torch.sigmoid(pred).detach().cpu().numpy().flatten()
-go_matrix = preprocess.get_go_matrix()
-assert transferrin_id in go_matrix.index
-assert len(go_matrix) == preprocess.get_vecs().shape[0]
-go_matrix["S"] = res.flatten()
-transferrin_index = go_matrix.index.get_loc(transferrin_id)
-print("Predictions done")
+parser.add_argument("--use_fuse", type=int, default=1)
+parser.add_argument("--use_model", type=int, default=1)
+parser.add_argument("--bs", type=int, default=16)
+parser.add_argument("--lr", type=float, default=0.001)
+parser.add_argument("--drop_out", type=float, default=0.3)
+parser.add_argument("--hidden_dim", type=int, default=512)
+parser.add_argument("--task_name", type=str, default="BACE")
+parser.add_argument("--fusion_name", type=str,
+                    default="data/pathbank/model/ProtBert-MolCLR-2-64-0.3-10-0.001-8192-0.0/")
+parser.add_argument("--m_model", type=str, default="ChemBERTa")
+parser.add_argument("--p_model", type=str, default="ProtBert")
+parser.add_argument("--max_no_improve", type=int, default=5)
+parser.add_argument("--n_layers", type=int, default=2)
+parser.add_argument("--metric", type=str, default="f1_max")
 
-print(f"Processing {p_model} - {m_model}")
-print("Transferrin:", res.flatten()[transferrin_index], sum(res > res[transferrin_index]))
 
-results = find_top_n_combinations(go_matrix, transferrin_index, n_results=30, max_cols=2, min_samples=100)
-for r in results:
-    print(r)
-    for go_id in r[2]['columns']:
-        print(get_go_description(go_id))
+def main(p_model, m_model, fuse_base, metric):
+    preprocess = PreprocessManager(p_model=p_model, reactome=True)
+    score, model = trainer_task_main(use_fuse=True, use_model=False, bs=16, lr=0.001, drop_out=0.3, hidden_dim=512,
+                                     task_name="BBBP", fuse_base=fuse_base, mol_emd=m_model, protein_emd=p_model,
+                                     n_layers=2, metric=metric, max_no_improve=5, return_model=True)
+    vecs = preprocess.get_vecs()
+    proteins = torch.tensor(vecs).to(device)
+    fuse_model: ReactEmbedModel = model.fuse_model
+    fuse_model.eval()
+    x = fuse_model.dual_forward(proteins, "P")
+    pred = model.layers(x)
+    res = torch.sigmoid(pred).detach().cpu().numpy().flatten()
+    go_matrix = preprocess.get_go_matrix()
+    assert transferrin_id in go_matrix.index
+    assert len(go_matrix) == preprocess.get_vecs().shape[0]
+    go_matrix["S"] = res.flatten()
+    transferrin_index = go_matrix.index.get_loc(transferrin_id)
+    results = find_top_n_combinations(go_matrix, transferrin_index, n_results=1, max_cols=2, min_samples=100)
+    results = results[0][2]
+    with open("transferrin/results.csv", "a") as f:
+        f.write(
+            f"{p_model},{m_model},{fuse_base},{metric},{results['filtered_size']},{results['new_rank']}\n")
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--p_model", type=str, default="ProtBert")
+    parser.add_argument("--m_model", type=str, default="ChemBERTa")
+    parser.add_argument("--fusion_name", type=str,
+                        default="data/reactome/model/ProtBert-MolCLR-2-256-0.3-10-5e-05-256-0.0/")
+    parser.add_argument("--metric", type=str, default="f1_max")
+    args = parser.parse_args()
+    torch.manual_seed(42)
+    main(args.p_model, args.m_model, args.fusion_name, args.metric)
