ddd85955a9a8d9d1f86520eb70ebdb931bb0a748
amitay.s <amitay.sich@gmail.com>
2025-01-05 18:24:03 +0200

    ReactEmbed - official implementation.

--- eval_tasks/prep_gearnet.py
+++ eval_tasks/prep_gearnet.py
@@ -2,8 +2,10 @@ from os.path import join as pjoin
 
 import torch
 from torchdrug import datasets
+from torchdrug import layers
 from torchdrug import models
 from torchdrug import transforms
+from torchdrug.layers import geometry
 
 from common.path_manager import data_path
 
@@ -24,71 +26,26 @@ def task_name_to_dataset_class(task_name):
     return getattr(datasets, task_name)
 
 
+gearnet_cp_file = "data/models/gearnet/mc_gearnet_edge.pth"
 gearnet_model = models.GearNet(input_dim=21, hidden_dims=[512, 512, 512, 512, 512, 512], num_relation=7,
                                edge_input_dim=59,
                                num_angle_bin=8,
                                batch_norm=True, concat_hidden=True, short_cut=True, readout="sum").to(device).eval()
+checkpoint = torch.load(gearnet_cp_file)
+gearnet_model.load_state_dict(checkpoint)
 
+graph_construction_model = layers.GraphConstruction(node_layers=[geometry.AlphaCarbonNode()],
+                                                    edge_layers=[
+                                                        geometry.SpatialEdge(radius=10.0, min_distance=5),
+                                                        geometry.KNNEdge(k=10, min_distance=5),
+                                                        geometry.SequentialEdge(max_distance=2)],
+                                                    edge_feature="gearnet")
 dataset = task_name_to_dataset_class(name)(output_base, transform=transform, atom_feature=None, bond_feature=None)
-
-
-class GearNet3Embedder:
-    def __init__(self, gearnet_cp_file="data/models/gearnet/mc_gearnet_edge.pth"):
-
-        from torchdrug import layers, data, transforms
-        from torchdrug.layers import geometry
-        self.data = data
-        self.transforms = transforms
-        self.fold_tokenizer = AutoTokenizer.from_pretrained("facebook/esmfold_v1")
-        self.fold_model = EsmForProteinFolding.from_pretrained("facebook/esmfold_v1")
-        self.fold_model = self.fold_model.to(device).eval()
-        checkpoint = torch.load(gearnet_cp_file)
-        self.gearnet_model.load_state_dict(checkpoint)
-        self.graph_construction_model = layers.GraphConstruction(node_layers=[geometry.AlphaCarbonNode()],
-                                                                 edge_layers=[
-                                                                     geometry.SpatialEdge(radius=10.0, min_distance=5),
-                                                                     geometry.KNNEdge(k=10, min_distance=5),
-                                                                     geometry.SequentialEdge(max_distance=2)],
-                                                                 edge_feature="gearnet")
-
-    def fold_seq(self, seq: str, output_io):
-        tokenized_input = self.fold_tokenizer([seq], return_tensors="pt", add_special_tokens=False)['input_ids']
-        tokenized_input = tokenized_input.to(device)
-        with torch.no_grad():
-            output = self.fold_model(tokenized_input)
-        pdbs = fold_to_pdb(output)
-        output_io.write(pdbs[0])
-
-    def to_vec(self, seq: str):
-        if len(seq) > 550:
-            seq = seq[:550]
-        try:
-            fold_tmp_io = io.StringIO()
-            self.fold_seq(seq, fold_tmp_io)
-            pdb_content = fold_tmp_io.getvalue()
-            fold_tmp_io.close()
-        except Exception as e:
-            print(e)
-            return None
-        if not pdb_content:
-            return None
-
-        mol = Chem.MolFromPDBBlock(pdb_content, sanitize=False)
-        if mol is None:
-            return None
-        try:
-            protein = self.data.Protein.from_molecule(mol)
-        except Exception as e:
-            print(e)
-            return None
-        truncate_transform = self.transforms.TruncateProtein(max_length=550, random=False)
-        protein_view_transform = self.transforms.ProteinView(view="residue")
-        transform = self.transforms.Compose([truncate_transform, protein_view_transform])
-        protein = {"graph": protein}
-        protein = transform(protein)
-        protein = protein["graph"]
-        protein = self.data.Protein.pack([protein])
-        protein = self.graph_construction_model(protein)
-        output = self.gearnet_model(protein.to(device), protein.node_feature.float().to(device))
-        output = output['node_feature'].mean(dim=0)
-        return output.detach().cpu().numpy().flatten()
+for data in dataset:
+    protein = data["graph"]
+    protein = data.Protein.pack([protein])
+    protein = graph_construction_model(protein)
+    output = gearnet_model(protein.to(device), protein.node_feature.float().to(device))['node_feature'].mean(dim=0)
+    output = output.cpu().detach().numpy()
+    print(output)
+    3 / 0
--- prep_all_seq.sh
+++ prep_all_seq.sh
@@ -4,38 +4,38 @@
 #SBATCH --requeue
 #SBATCH --array=1-30
 #task_names=("BACE BBBP CEP ClinTox Delaney FreeSolv HIV Lipophilicity Malaria SIDER Tox21 BetaLactamase Fluorescence Stability Solubility BinaryLocalization SubcellularLocalization EnzymeCommission GOMF GOBP GOCC SecondaryStructure HumanPPI YeastPPI PPIAffinity BindingDB PDBBind DrugBank Davis")
-configs="--task_name BACE|\
---task_name BBBP|\
---task_name CEP|\
---task_name ClinTox|\
---task_name Delaney|\
---task_name FreeSolv|\
---task_name HIV|\
---task_name Lipophilicity|\
---task_name Malaria|\
---task_name SIDER|\
---task_name Tox21|\
---task_name BetaLactamase|\
---task_name Fluorescence|\
---task_name Stability|\
---task_name Solubility|\
---task_name BinaryLocalization|\
---task_name SubcellularLocalization|\
---task_name EnzymeCommission|\
---task_name GOMF|\
---task_name GOBP|\
---task_name GOCC|\
---task_name SecondaryStructure|\
---task_name HumanPPI|\
---task_name YeastPPI|\
---task_name PPIAffinity|\
---task_name BindingDB|\
---task_name PDBBind|\
---task_name DrugBank|\
---task_name Davis"
+configs="BACE|\
+BBBP|\
+CEP|\
+ClinTox|\
+Delaney|\
+FreeSolv|\
+HIV|\
+Lipophilicity|\
+Malaria|\
+SIDER|\
+Tox21|\
+BetaLactamase|\
+Fluorescence|\
+Stability|\
+Solubility|\
+BinaryLocalization|\
+SubcellularLocalization|\
+EnzymeCommission|\
+GOMF|\
+GOBP|\
+GOCC|\
+SecondaryStructure|\
+HumanPPI|\
+YeastPPI|\
+PPIAffinity|\
+BindingDB|\
+PDBBind|\
+DrugBank|\
+Davis"
 IFS='|' read -ra config_array <<< "$configs"
 eval "$(conda shell.bash hook)"
 conda activate retd
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 task_name=${config_array[$((SLURM_ARRAY_TASK_ID - 1))]}
-python eval_tasks/prep_tasks_seqs.py "$task_name"
+python eval_tasks/prep_tasks_seqs.py --task_name "$task_name"
