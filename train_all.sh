ceea8a5589e10ddce407e2962477ae61f2b86ff4
amitay.s <amitay.sich@gmail.com>
2025-01-05 10:51:41 +0200

    ReactEmbed - official implementation.

--- train_all.sh
+++ train_all.sh
@@ -26,10 +26,10 @@ eval "$(conda shell.bash hook)"
 
 conda activate ReactEmbedESM
 export PYTHONPATH=$PYTHONPATH:$(pwd)
-python contrastive_learning/trainer.py $config
-python contrastive_learning/trainer.py $config --flip_prob 0.1
-python contrastive_learning/trainer.py $config --flip_prob 0.5
-python contrastive_learning/trainer.py $config --data_name "pathbank"
-python contrastive_learning/trainer.py $config --n_layers 2 --epochs 10
-python contrastive_learning/trainer.py $config --min_value 1
-python contrastive_learning/trainer.py $config --min_value 10
+python contrastive_learning/trainer.py $config --override
+python contrastive_learning/trainer.py $config --flip_prob 0.1 --override
+python contrastive_learning/trainer.py $config --flip_prob 0.5 --override
+python contrastive_learning/trainer.py $config --data_name "pathbank" --override
+python contrastive_learning/trainer.py $config --n_layers 2 --epochs 10 --override
+python contrastive_learning/trainer.py $config --min_value 1 --override
+python contrastive_learning/trainer.py $config --min_value 10 --override
