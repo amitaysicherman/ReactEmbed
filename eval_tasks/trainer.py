ec9b45d2c62e00a330a7387af00f44dda6c36e88
amitay.s <amitay.sich@gmail.com>
2025-01-07 16:59:56 +0200

    ReactEmbed - official implementation.

--- eval_tasks/trainer.py
+++ eval_tasks/trainer.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
     parser.add_argument("--use_model", type=int, default=1)
     parser.add_argument("--bs", type=int, default=16)
     parser.add_argument("--lr", type=float, default=0.001)
-    parser.add_argument("--drop_out", type=float, default=0.3)
+    parser.add_argument("--drop_out", type=float, default=0.0)
     parser.add_argument("--hidden_dim", type=int, default=512)
     parser.add_argument("--task_name", type=str, default="BACE")
     parser.add_argument("--fusion_name", type=str,
